cefb2dd1ba66ff90373e37d4b06665baac534667
KeyPS8 <key87luna@gmail.com>
2026-02-17 13:01:04 -0600

    Fix camera position calculation

--- main.js
+++ main.js
@@ -39,12 +39,10 @@ function update(){
     if(keys["a"]||keys["ArrowLeft"]) player.x-=player.speed;
     if(keys["d"]||keys["ArrowRight"]) player.x+=player.speed;
 
-camera.x = player.x - canvas.width/2;
+camera.x = player.x + canvas.width/2;
 camera.y = player.y - canvas.height/2;
 
 }
-
-
 
 function drawWorld(){
 ctx.fillStyle="#1a1a1a";
